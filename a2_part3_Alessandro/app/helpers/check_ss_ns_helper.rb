25b683d1eaee01a031d10e58c28976636e63f13a
Alessandro <131570714+Aletexx@users.noreply.github.com>
2024-12-17 12:05:47 +1100

    Add files via upload
    The user will provide the input through web page in rails. Web page should contain a text field (Root directory) and a search button in word files.
    Search results will be display on web page as well.
    In this Submission for every SSN found, the filename including the full path will be printed to the browser and the content of the document will be printed into a text. Please note the following picture is just an example giving you hints how to achieve the interface. Your interface should show the path and content of word files.

---
+++ a2_part3_Alessandro/app/helpers/check_ss_ns_helper.rb
@@ -0,0 +1,2 @@
+module CheckSsNsHelper
+end
---
+++ a2_part3_Alessandro/config/routes.rb
@@ -0,0 +1,20 @@
+Rails.application.routes.draw do
+  get "static_pages/home"
+
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", as: :rails_health_check
+  # Render dynamic PWA files from app/views/pwa/*
+  get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+  get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+
+  # route for the search action.
+  get 'search', to: 'static_pages#search'
+
+  # route for processing upload action
+  post 'upload', to: 'static_pages#upload'
+
+  root 'static_pages#home'
+
+end
